cce8535bacb2e1128ee165e83a1e53e6c76b4295
Davide Dell'Erba <delda2@hotmail.it>
2014-07-25 12:30:39 +0200

    FrogJmp: test score 100%

---
+++ FrogJmp.php
@@ -0,0 +1,17 @@
+<?php
+
+$X = 10;
+$Y = 85;
+$D = 30;
+
+function solution($X, $Y, $D) {
+    $mod  = ($Y - $X) % $D;
+    $diff = (int)(($Y - $X) / $D);
+    if($mod > 0)
+        $diff++;
+
+    return $diff;
+}
+
+echo solution($X, $Y, $D);
+echo "\n";
