8fa47e4b7924fa6d4f92ed5fd25a9341fd68560c
Davide Dell'Erba <delda2@hotmail.it>
2014-09-05 17:57:25 +0200

    Codility - Lesson 1: solution to PermMissingElem in PHP (Test score: 100%)

--- PermMissingElem.php
+++ PermMissingElem.php
@@ -1,12 +1,13 @@
 <?php
 
+// example
 $A = array(2,3,1,5);
+// expected: 4
 
 function solution($A) {
     $sizeOfA = sizeof($A);
     $rightSum = (($sizeOfA + 1) * ($sizeOfA + 2)) / 2;
     $sumOfValues = $rightSum;
-    // volevo utilizzare l'array_sum ma avevo problemi di max_size
     for($i=0; $i<$sizeOfA; $i++)
         $sumOfValues -= $A[$i];
 
