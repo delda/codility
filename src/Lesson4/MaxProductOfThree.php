5f11369aa5b62887e14582b204cb6ca8caff3aae
Davide Dell'Erba <davide.dellerba@immobiliare.it>
2015-01-30 16:16:30 +0100

    Codility - Lesson 4: solution to MaxProductOfThree in PHP (Test score: 100%)

--- src/Lesson4/MaxProductOfThree.php
+++ src/Lesson4/MaxProductOfThree.php
@@ -1,32 +1,12 @@
 <?php
 
-error_reporting(E_NOTICE);
+/**
+ * MaxProductOfThree
+ *
+ *  Maximize A[P] * A[Q] * A[R] for any triplet (P, Q, R).
+ */
 
-// example
-$A = array(-3,1,2,-2,5,6);
-// expected: 60
-
-// one_triple
-// three elements
-//$A = array(-10,-2,-4);
-// expected: -80
-
-// simple1
-// simple tests
-//$A = array(4,7,3,2,1,-3,-5);
-// expected: 105
-
-// simple2
-// simple tests
-//$A = array(-5,5,-5,4);
-// expected: 125
-
-// medium_range
-// -1000, -999, ... 1000, length = ~1,000
-//for($i=-1000; $i<1001; $i++){
-//    $A[] = $i;
-//}
-// expected: 999.000.000
+include '../../Tests.class.php';
 
 function solution($A) {
     $sizeOfA = sizeof($A);
@@ -35,5 +15,35 @@ function solution($A) {
     return $max;
 }
 
-echo solution($A);
-echo "\n";
+$test = new Tests('MaxProductOfThree');
+
+// example
+$A = array(-3, 1, 2, -2, 5, 6);
+$result = 60;
+$test->run(array($A), $result);
+
+// one_triple
+// three elements
+$A = array(-10, -2, -4);
+$result = -80;
+$test->run(array($A), $result);
+
+// simple1
+// simple tests
+$A = array(4,7,3,2,1,-3,-5);
+$result = 105;
+$test->run(array($A), $result);
+
+// simple2
+// simple tests
+$A = array(-5, 5, -5, 4);
+$result = 125;
+$test->run(array($A), $result);
+
+// medium_range
+// -1000, -999, ... 1000, length = ~1,000
+for($i=-1000; $i<1001; $i++){
+    $A[] = $i;
+}
+$result = 999000000;
+$test->run(array($A), $result);
