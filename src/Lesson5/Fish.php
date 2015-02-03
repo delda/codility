c921c8926ea5a3bbbe9494a44ef0f076db306651
Davide Dell'Erba <davide.dellerba@immobiliare.it>
2015-02-03 15:54:28 +0100

    Codility - Lesson 5: solution to Fish in PHP (Test score: 100%)

--- src/Lesson5/Fish.php
+++ src/Lesson5/Fish.php
@@ -1,6 +1,12 @@
 <?php
 
-include 'Tests.class.php';
+/**
+ * Fish
+ *
+ *  N voracious fish are moving along a river. Calculate how many fish are alive.
+ */
+
+include '../../Tests.class.php';
 
 function solution($A, $B) {
     $sizeOfA = sizeof($A);
@@ -31,25 +37,25 @@ $test = new Tests('Fish');
 $A = array(4,3,2,1,5);
 $B = array(0,1,0,0,0);
 $result = 2;
-$test->run(array($A, $B), $result);
+$test->run(array($A, $B), $result, 'example');
 
 // extreme_small
 // 1 or 2 fishes
 $A = array(999999999,1000000000);
 $B = array(1,0);
 $result = 1;
-$test->run(array($A, $B), $result);
+$test->run(array($A, $B), $result, 'extreme_small');
 
 // simple1
 // simple test
 $A = array(8,6,5,3,2,4,7);
 $B = array(1,1,1,0,0,1,1);
 $result = 5;
-$test->run(array($A, $B), $result);
+$test->run(array($A, $B), $result, 'simple1');
 
 // simple2
 // simple test
 $A = array(8,6,5,3,2,4,7);
 $B = array(1,1,1,1,1,0,0);
 $result = 1;
-$test->run(array($A, $B), $result);
+$test->run(array($A, $B), $result, 'simple2');
