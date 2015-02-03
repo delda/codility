5003042bec4031d94d3b3ae8ed8fdb0ed14e6f3f
Davide Dell'Erba <davide.dellerba@immobiliare.it>
2015-02-03 15:55:56 +0100

    Codility - Lesson 5: solution to Nesting in PHP (Test score: 100%)

--- src/Lesson5/Nesting.php
+++ src/Lesson5/Nesting.php
@@ -1,19 +1,23 @@
 <?php
 
-include 'Tests.class.php';
+/**
+ * Nesting
+ *
+ *  Determine whether given string of parentheses is properly nested.
+ */
+
+include '../../Tests.class.php';
 
 function solution($S) {
     $sizeOfA = strlen($S);
     $i = 0;
     $sumOfBrackets = 0;
     while($i < $sizeOfA){
-//        var_dump("# $S[$i]");
         if($S[$i] == '('){
             $sumOfBrackets++;
         }else{
             $sumOfBrackets--;
         }
-//        var_dump($sumOfBrackets);
         if($sumOfBrackets < 0){
             return 0;
         }
@@ -30,10 +34,10 @@ $test = new Tests('Nesting');
 // example test 1
 $A = '(()(())())';
 $result = 1;
-$test->run($A, $result);
+$test->run($A, $result, 'example1');
 
 // example2
 // example test 2
 $A = '())';
 $result = 0;
-$test->run($A, $result);
+$test->run($A, $result, 'example2');
