6f8cf9ddc3f971ca4f91fea05ecc9e9e1afbe663
Davide Dell'Erba <delda2@hotmail.it>
2016-02-17 15:29:41 +0100

    Codility - Lesson 1: solution to PermMissingElem in PHP (Test score: 100%)

---
+++ src/Lesson3/PermMissingElem.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * PermMissingElem
+ *
+ *  Find the missing element in a given permutation.
+ */
+
+include '../../Tests.class.php';
+
+function solution($A) {
+    $sizeOfA = sizeof($A);
+    $rightSum = (($sizeOfA + 1) * ($sizeOfA + 2)) / 2;
+    $sumOfValues = $rightSum;
+    for($i=0; $i<$sizeOfA; $i++)
+        $sumOfValues -= $A[$i];
+
+    return intval($sumOfValues);
+}
+
+$test = new Tests('PermMissingElem');
+
+$A = array(2,3,1,5);
+$result = 4;
+$test->run(array($A), $result);
