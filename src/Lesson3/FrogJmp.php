4ed0689d129f8f3e230bdeb7e498166d7ba9cec4
Davide Dell'Erba <delda2@hotmail.it>
2016-02-17 15:28:37 +0100

    Codility - Lesson 3: solution to FrogJmp in PHP (Test score: 100%)

---
+++ src/Lesson3/FrogJmp.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * FrogJmp
+ *
+ *  Count minimal number of jumps from position X to Y.
+ */
+
+include '../../Tests.class.php';
+
+function solution($X, $Y, $D) {
+    $mod  = ($Y - $X) % $D;
+    $diff = (int)(($Y - $X) / $D);
+    if($mod > 0)
+        $diff++;
+
+    return $diff;
+}
+
+$test = new Tests('FrogJmp');
+
+$X = 10;
+$Y = 85;
+$D = 30;
+$result = 3;
+$test->run(array($X, $Y, $D), $result);
